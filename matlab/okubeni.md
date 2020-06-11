0223bf836d50e3f70a64c2f9bd398a19456c9ed9
mustafa <mus19f1a@hotmail.com>
2020-06-12 01:14:51 +0300

    kodlama düzenlendi, açıklama eklendi

---
+++ matlab/okubeni.md
@@ -0,0 +1,15 @@
+## Bu dizin içerisinde yer alan kodlamalar ve açıklamaları
+
+# Poligondaki rastgele bir noktadan gelen düzen [t1.m]
+Twitter üzerinde gördüğüm bir paylaşıma istinaden MATLAB üzerinde t1.m dosyasını oluşturup sonuçları incelediğimizde aşağıdaki görüntü bizleri karşılıyor :)
+
+<blockquote class="twitter-tweet"><p lang="en" dir="ltr">a simple and random process that creates an intricate and structured fractal pattern. At each step, pick a random edge to form a triangle, and the next point is the center of the triangle. <a href="https://t.co/yLxmdZQCWY">pic.twitter.com/yLxmdZQCWY</a></p>&mdash; Matt Henderson (@matthen2) <a href="https://twitter.com/matthen2/status/1268808515574886407?ref_src=twsrc%5Etfw">June 5, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+
+Bu örnek eşkanar altıgen için 100 bin adet rastgele nokta seçilerek üretilmiştir.
+
+![recursive shape](icerik/t1g1.gif)
+
+Kodlama üzerinden poligonun köşe sayısını değiştirdiğinizde benzer şekilde iç içe kopya görüntülerin oluştuğunu görebilirsiniz. Gerçekten ilginç :)
+
+Bir sonraki sefere bu olayın matematiksel altyapısını irdeleyelim mi ne dersiniz?
--- matlab/t1.m
+++ matlab/t1.m
@@ -1,9 +1,18 @@
+% sürüm : v0.01 - 12.haziran.2020, s00:40
+%% Parametreler
 % Başlangıç poligonunu oluşturuyoruz
 bslngcX = [6, -6, -12, -6, 6, 12];
 h = sqrt(108);
 bslngcY = [h, h, 0, -h, -h, 0];
 
-altgn = [bslngcX;bslngcY]';
+poligon = [bslngcX;bslngcY]';
+% Hesaplanacak nokta sayısını seçiyoruz
+say = 100000;
+
+%% Hesaplama Kısmı
+% poligonun köşe sayısı
+ps = size(poligon,1);
+psr = ps-1;
 
 % Figür çerçevesini ayarlıyoruz
 figure('Position', [100, 100, 900, 900]);
@@ -17,8 +26,7 @@ scatter(bslngcX,bslngcY);
 % (Altıgen içerisinde herhangi bir nokta olabilir)
 p0 = [3,4];
 
-% Hesaplanacak nokta sayısını seçiyoruz
-say = 100000;
+
 % değişkenimiz için hafızada yer ayarlıyoruz
 % (sıfırlardan oluşan bir matris)
 snc = zeros(say,2);
@@ -26,17 +34,17 @@ snc = zeros(say,2);
 for i = 1:say
     
   % rastgele bir sayı seçiyoruz, (0 ile 5 arasında)
-    rst = randi(5)+randi(5);
+    rst = randi(psr)+randi(psr);
   % seçilen sayıya göre bir kenar ve takip eden kenarı seçiyoruz
-    p1 = altgn(mod(rst,6)+1,:);
-    p2 = altgn(mod(rst+1,6)+1,:);
+    p1 = poligon(mod(rst,ps)+1,:);
+    p2 = poligon(mod(rst+1,ps)+1,:);
   % elde edilen üçgen kenarlarına göre merkez noktasını hesaplıyoruz
     p0 = mean([p0;p1;p2]);
   % hesaplanan merkez noktasını kaydediyoruz
     snc(i,:) = p0;
     
 end
-
+%% Çizim Kısmı
 % Noktaları figüre eklemek için "hold on" seçeneğini aktifleştiriyoruz
 hold on;
 % Hesaplanan tüm merkez noktalarını çizdiriyoruz
